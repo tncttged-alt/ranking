93587d99087335707764c5185448de551c98b9dd
tncttged-alt <tnct.tged@gmail.com>
2026-08-14 09:40:59 +0900

    Delete tools directory

--- tools/build_make_article.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Build standalone EXE for make_article.py using pyinstaller
-if not exist venv (py -3 -m venv venv && venv\Scripts\pip.exe install --upgrade pip)
-call venv\Scripts\activate
-python -m pip install pyinstaller
-pyinstaller --onefile --distpath dist --workpath build --specpath build make_article.py
-echo Build finished. See dist\make_article.exe
-pause
